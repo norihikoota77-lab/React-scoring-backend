95b9505abe1d65d507c26db9cfd480895e95c6b5
Norihiko Ota <norihikoota77@gmail.com>
2026-05-28 10:19:38 +0900

    add Vercel URL to CORS

--- keiba_app/settings.py
+++ keiba_app/settings.py
@@ -133,6 +133,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://react-scoring-frontend.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
